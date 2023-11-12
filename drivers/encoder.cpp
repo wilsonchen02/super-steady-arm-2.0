91ec7dda7998f6d1a6fdc2260262d2abb7a688d7
Jaccob M Bell <jaccob@umich.edu>
2023-11-12 15:14:34 -0500

    Encoder framework

---
+++ drivers/encoder.cpp
@@ -0,0 +1,18 @@
+
+float Encoder::get_position_change() {
+    uint16_t current_counts = this->m_tim->CNT;
+    int32_t delta = current_counts - this->m_prev_counts;
+    this->m_prev_counts = current_counts;
+
+    //TODO: convert from counts to position somehow
+    return static_cast<float>(delta);
+}
+
+void Encoder::get_current_position() {
+    // TODO: Might not need. Check what auto code generation does for us
+    // Initalize TIM->CNT to some value?
+    // Initalize the channels/timer in encoder mode
+    
+
+
+}
---
+++ drivers/encoder.h
@@ -0,0 +1,23 @@
+// TODO: Change to be actual TIM
+constexpr TIM_HandleTypeDEF ENCODER_HTIM = htim2;
+constexpr TIM_HandleTypeDEF ENCODER_TIM = TIM2;
+
+/* TODO: 
+ * Do we want to implement position using the encoder?
+ * Or is just getting change in position good?
+ 
+ * This outcoder changes output on Z once per revolution.
+ * Could possibly use that to calculate absolute position? 
+ * But also our encoder is never going to rotate a full cycle. 
+ */
+
+class Encoder {
+    public:
+    void init();
+    float get_position_change();
+
+    private:
+    TIM_HandleTypeDEF m_handler = ENCODER_HTIM;
+    TIM_TypeDef* m_tim = ENCODER_TIM;
+    uint16_t m_prev_counts;
+};
